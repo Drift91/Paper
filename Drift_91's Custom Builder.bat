40b777cfbf25582b29cce6180751700eea487b24
Kyle J. McKeown <mckeown_k@hotmail.com>
2021-12-28 03:47:31 -0500

    Use correct command for `echo`.

--- Drift_91's Custom Builder.bat
+++ Drift_91's Custom Builder.bat
@@ -1,10 +1,12 @@
 @echo off
 cd /d "S:\Contents\To Lotus\downloads\Software & Mods\#GitHub\Paper"
 
-@rem Cleaning and resetting repository...
+echo Cleaning and resetting repository...
 git clean -fdxq && git reset --hard
 
-@rem Starting gradle...
+TIMEOUT 3
+
+echo Starting gradle...
 gradlew.bat applyPatches --no-daemon && gradlew.bat createReobfBundlerJar --no-daemon
 
 pause
