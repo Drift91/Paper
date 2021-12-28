3dcba06feaef0b819534cf6b2b2320df01bc54f0
Kyle J. McKeown <mckeown_k@hotmail.com>
2021-12-28 03:42:41 -0500

    Clean up and fancify custom builder.

--- Drift_91's Custom Builder.bat
+++ Drift_91's Custom Builder.bat
@@ -1,7 +1,10 @@
+@echo off
 cd /d "S:\Contents\To Lotus\downloads\Software & Mods\#GitHub\Paper"
 
+@rem Cleaning and resetting repository...
 git clean -fdxq && git reset --hard
 
+@rem Starting gradle...
 gradlew.bat applyPatches --no-daemon && gradlew.bat createReobfBundlerJar --no-daemon
 
 pause
