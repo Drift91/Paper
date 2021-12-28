ce64c14b046ba062e5259dbf9d43579138b64ded
Kyle J. McKeown <mckeown_k@hotmail.com>
2021-12-28 03:50:43 -0500

    Remove useless `timeout`

--- Drift_91's Custom Builder.bat
+++ Drift_91's Custom Builder.bat
@@ -4,8 +4,6 @@ cd /d "S:\Contents\To Lotus\downloads\Software & Mods\#GitHub\Paper"
 echo Cleaning and resetting repository...
 git clean -fdxq && git reset --hard
 
-TIMEOUT 3
-
 echo Starting gradle...
 gradlew.bat applyPatches --no-daemon && gradlew.bat createReobfBundlerJar --no-daemon
 
