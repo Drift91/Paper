b4c6e0024087fb46f828ef70901478bd714d7dec
Kyle J. McKeown <mckeown_k@hotmail.com>
2022-05-18 02:30:17 -0400

    Add additional `-f` argument in `git clean` command for custom builder.

--- Drift_91's Custom Builder.bat
+++ Drift_91's Custom Builder.bat
@@ -2,7 +2,7 @@
 cd /d "S:\Contents\To Lotus\downloads\Software & Mods\#GitHub\Paper"
 
 echo Cleaning and resetting repository...
-git clean -fdxq && git reset --hard
+git clean -ffdxq && git reset --hard
 
 echo Starting gradle...
 gradlew.bat applyPatches --no-daemon && gradlew.bat createReobfBundlerJar --no-daemon
